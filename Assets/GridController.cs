d832f9a1e98cf1bf2a1dc80aa171165c051898d1
Trey G <Trey G>
2017-03-02 20:43:09 -0600

    Added old grid script contoller and made hex tile prefab

---
+++ Assets/GridController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridController : MonoBehaviour {
+    public GameObject hexPrefab;
+
+    int width = 12;
+    int height = 20;
+
+    float xOffset = 0.47f;
+    float yOffset = 0.332f;
+
+	// Use this for initialization
+	void Start () {
+	
+        for(int x = 0; x < width; x++) { 
+            for(int y = 0; y < height; y++)
+            {
+                float xPos = (transform.localPosition.x + x) * xOffset;
+                float yPos = (transform.localPosition.y + y) * yOffset;
+             
+                if(y % 2 == 1)
+                {
+                    xPos += xOffset / 2f;    
+                }
+                GameObject hex_go = (GameObject)Instantiate(hexPrefab, new Vector3(xPos, yPos,-5), Quaternion.identity);
+                hex_go.name = "hex " + x + "_" + y;
+                hex_go.transform.SetParent(this.transform); 
+       
+            }     
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
